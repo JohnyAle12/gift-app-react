f71141e5fbd1fa2c5fe16ed78a919dc941d5d0a2
Johny Prieto <johny.prieto@munitienda.com>
2022-11-30 10:04:15 -0500

    add loading validation

--- src/components/GifGrid.tsx
+++ src/components/GifGrid.tsx
@@ -12,6 +12,11 @@ const GifGrid = ({category}:Props) => {
     return (
         <>
             <h3>{category}</h3>
+            
+            {isLoading && (
+                <h2>Cargando ...</h2>
+            )}
+            
             <div className='card-grid'>
                 { images.map( (image, index) => <GifItem key={index} { ...image } />) }
             </div>
